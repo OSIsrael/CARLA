9afa5c045566f37a94ca2d1c2fbc874264bc5355
OSIsrael <israelos067@gmail.com>
2023-12-28 17:32:03 -0600

    Reinicializar el repositorio

---
+++ src/app/inses/inses.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inses',
+  templateUrl: './inses.page.html',
+  styleUrls: ['./inses.page.scss'],
+})
+export class InsesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
